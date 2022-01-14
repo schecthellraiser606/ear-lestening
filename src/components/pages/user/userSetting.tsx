050bb7b299b37558c4318213241ac2866a260a20
schectman666 <89838264+schecthellraiser606@users.noreply.github.com>
2022-01-15 03:24:07 +0900

    usersettingBase

--- src/components/pages/user/userSetting.tsx
+++ src/components/pages/user/userSetting.tsx
@@ -1,7 +1,34 @@
-import {memo, VFC} from "react";
+import { Box, Divider, Flex, FormControl, Heading, Input, Stack } from "@chakra-ui/react";
+import {ChangeEvent, memo, useState, VFC} from "react";
+import { getAuth, updateProfile } from "firebase/auth";
+import { TopLayout } from "../../organisms/layout/TopLayout";
 
 export const UserSetting: VFC = memo( ()=> {
+  const auth = getAuth();
+  const signInUser = auth.currentUser;
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const onChangeName = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+
   return(
-    <p>userページです</p>
+    <TopLayout>
+    <Flex align="center" justify="center" height="100vh">
+    <Box bg="white" padding={6} >
+    <Heading fontSize="2xl">ユーザ情報</Heading>
+    <Divider my={4}/>
+
+    <Stack textAlign="center">
+      <FormControl>ユーザ名</FormControl>
+      <Input value={ signInUser?.displayName || "" } onChange={onChangeName} isReadOnly={false}/>
+      <FormControl>Email Address</FormControl>
+      <Input value={ signInUser?.email || "" } onChange={onChangeEmail} isReadOnly={false}/>
+    </Stack>
+
+    </Box>
+    </Flex>
+    </TopLayout>
   );
 });
